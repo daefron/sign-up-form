10f34925d69216fc6fb2bbe354e70e5cfe952b6c
Daefron <thomas_evans@outlook.com>
2023-05-14 19:37:17 +1000

    Fixed password verification, style changes.

--- code.js
+++ code.js
@@ -1,11 +1,22 @@
-const button = document.getElementById('but');
+var pp = document.getElementById('pp');
+var button = document.getElementById('but');
 button.addEventListener('click', () => {
-    const confirm = document.getElementById('confirm').value;
-    const password = document.getElementById('password').value;
-    if (confirm !== password) {
-        const pp = document.getElementById('pp');
+    var confirm = document.getElementById('confirm');
+    var password = document.getElementById('password');
+    if (confirm.value !== password.value) {
         pp.style['color'] = 'black';
-        document.getELementById('confirm').style['outline'] = 'none';
-        document.getELementById('confirm').style['border'] = '1px solid black';
+        confirm.style['outline'] = 'none';
+        confirm.style['border'] = '2px solid black';
+        password.style['outline'] = 'none';
+        password.style['border'] = '2px solid black';
+
+    }
+    else {
+        pp.style['color'] = 'white';
+        confirm.style['outline'] = 'none';
+        confirm.style['border'] = '1px solid rgb(228,228,228)';
+        password.style['outline'] = 'none';
+        password.style['border'] = '1px solid rgb(228,228,228)';
+
     }
 })
